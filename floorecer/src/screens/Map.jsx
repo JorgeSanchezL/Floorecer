d92e0aaacf65858c67685f0a50fa467dfa497f5c
Jorge Sánchez Lorente <jsanlor@upv.edu.es>
2022-10-11 12:29:57 +0200

    user location displayed

--- floorecer/src/screens/Map.jsx
+++ floorecer/src/screens/Map.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps'
+import * as Location from 'expo-location';
 
 const Map = () => {
   const markerList = getAllMarkers() 
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} showsPointsOfInterest={false} customMapStyle={mapStyle} >
+      <MapView 
+        style={styles.map} 
+        showsPointsOfInterest={false}
+        customMapStyle={mapStyle}
+        onMapReady={() => askLocationPermissions()}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true} showsMyLocationButton
+      >
         {markerList.map((element, _) => {
             return <Marker
                 key={element.key}
@@ -18,6 +26,13 @@ const Map = () => {
       </MapView>
     </View>
   );
+}
+
+const askLocationPermissions = async () => {
+  let { status } = await Location.requestForegroundPermissionsAsync();
+  if (status !== 'granted') {
+    console.log('Permission to access location was denied');
+  }
 }
 
 const styles = StyleSheet.create({
