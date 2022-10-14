ef29b0003e45b1086aa0f3b547cd6fcf84cc5afd
Jorge Sánchez Lorente <jsanlor@upv.edu.es>
2022-10-14 09:56:44 +0200

    database access created

--- floorecer-backend/app.js
+++ floorecer-backend/app.js
@@ -2,11 +2,34 @@ const express = require('express');
 
 const app = express();
 const PORT = 3000;
+const AWS = require('aws-sdk')
+
+AWS.config.loadFromPath('./secrets/config.json');
+
+const client = new AWS.DynamoDB.DocumentClient();
 
 app.get('/', (req, res)=>{
 	res.status(200);
 	res.send("Welcome to root URL of Server");
 });
+
+app.get('/poi/all', (req, res) => {
+	var params = {
+        TableName: 'POI'
+    };
+
+    client.scan(params, (err, data) => {
+        if (err) {
+            res.status(404)
+			res.send(err)
+			console.log(err)
+        } else {
+            res.contentType = 'application/json';
+			res.status(200)
+            res.send(data.Items);
+        }
+    });
+})
 
 app.listen(PORT, (error) =>{
 	if(!error)
