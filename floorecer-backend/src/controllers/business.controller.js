aab1e20b172b5dfd2334a911916c403dc07d20c4
delniss <De12li13a14@gmail.com>
2022-10-23 17:37:32 +0200

    configure business backend

--- floorecer-backend/src/controllers/business.controller.js
+++ floorecer-backend/src/controllers/business.controller.js
@@ -2,7 +2,7 @@ import {auth,database,app} from '../../firebase.js';
 import {signInWithCustomToken,signInWithEmailAndPassword,createUserWithEmailAndPassword} from "firebase/auth";
 import { async } from '@firebase/util';
 import {  getDocs } from 'firebase/firestore/lite';
-import { doc,setDoc,GeoPoint,collection } from 'firebase/firestore';
+import { doc,setDoc,GeoPoint,collection, getDoc,updateDoc } from 'firebase/firestore';
 
 export const newBusiness = async(req,res) => {
     const{owner,name,nif,direction,latitude,longitude,openingHours,category} = req.body
@@ -18,4 +18,36 @@ export const newBusiness = async(req,res) => {
         category: category,
     });
 
+}
+export const getBusiness = async(req,res) => {
+    console.log("get business")
+    console.log(req.body)
+    try {
+        const docRef = doc(database, "business", "VWFVMwQ5Q2gxpRENlPbS");
+        console.log(JSON.stringify(docRef))
+        const docSnap = await getDoc(docRef);
+        console.log(docSnap.data());
+        if (docSnap.exists()) {
+            console.log("Document data:", docSnap.data());
+            res.json(docSnap.data())
+        } else {
+            console.log("No such document!");
+            res.json(null)
+        }
+    } catch(error) {
+        console.log(error)
+    }
+    
+}
+
+export const updateBusiness = async (req, res) => {
+    const { uid, body } = req.body;
+
+    try {
+        const docRef = doc(database, 'business', uid);
+        await updateDoc(docRef, body);
+        res.json({saved: true});
+    } catch (e) { 
+        res.status(500).json({saved: false});
+    }
 }
--- floorecer-backend/src/routes/business.routes.js
+++ floorecer-backend/src/routes/business.routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
-import { newBusiness } from '../controllers/business.controller.js';
+import { newBusiness, getBusiness,updateBusiness } from '../controllers/business.controller.js';
 
 const router = Router();
 
 router.post('/newBusiness',newBusiness);
+router.get('/getBusiness',getBusiness);
+router.post('/updateBusiness',updateBusiness);
+
 
 export default router;
