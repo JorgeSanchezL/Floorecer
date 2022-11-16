c1f075c4e23bac0ab661219947b5b37abb299dad
Alfonso Xiwen Chen <axchen@upv.edu.es>
2022-11-16 10:35:35 +0100

    merge

--- floorecer-backend/src/controllers/authentication.controller.js
+++ floorecer-backend/src/controllers/authentication.controller.js
@@ -48,22 +48,6 @@ export const register = async (req,res) => {
     .then((userCredential) => {
         // Signed in
     setDoc(doc(database,"users",userCredential.user.uid) ,{
-<<<<<<< HEAD
-    username: username,
-    usernameForSearch: usernameForSearch,
-    points : 0,
-    followers: [],
-    following: [],
-    isBusinessOwner : isBusinessOwner,
-    numero : numberphone,
-    email:email,
-    password:password,
-    garden: [{type: "noflower", petals: 3, health: 3}, {type: "noflower", petals: 3, health: 3}, {type: "noflower", petals: 3, health: 3}, {type: "noflower", petals: 3, health: 3}],
-    profileImage : 'testing.png',
-    historico : [],
-    item:{}
-
-=======
       username: username,
       usernameForSearch: usernameForSearch,
       points : 0,
@@ -77,7 +61,6 @@ export const register = async (req,res) => {
       garden: [{type: "noflower", petals: 3, health: 3}, {type: "noflower", petals: 3, health: 3}, {type: "noflower", petals: 3, health: 3}, {type: "noflower", petals: 3, health: 3}],
       profileImage : 'testing.png',
       historico : [],
->>>>>>> 88788578dbdf37b8f00ac524144831e197e6520d
     }
   )
 
