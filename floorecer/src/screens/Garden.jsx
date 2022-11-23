08ad10cb47b057e5af57df3ab8928228ad1e6a90
Jorge Sánchez Lorente <jsanlor@upv.edu.es>
2022-11-23 11:01:54 +0100

    Inicio nuevo jardin

--- floorecer/src/screens/Garden.jsx
+++ floorecer/src/screens/Garden.jsx
@@ -4,13 +4,20 @@ import { getItemAsync } from 'expo-secure-store';
 import BotonFlor from '../components/BotonFlor';
 import BotonImagen from '../components/BotonImagen'
 import CustomButton from '../components/CustomButton'
-import grass from '../../assets/garden/grass.jpg'
 import close from '../../assets/close.png'
 
 import { BACKEND_URL } from '@env';
 
 //Imports de las flores
-import dirt from '../../assets/garden/dirt.jpg'
+import addIcon from '../../assets/garden/addIcon.svg'
+
+import deadCactus from '../../assets/garden/deadCactus.svg'
+import cactus0 from '../../assets/garden/cactus0.svg'
+import cactus1 from '../../assets/garden/cactus1.svg'
+import cactus2 from '../../assets/garden/cactus2.svg'
+import cactus3 from '../../assets/garden/cactus3.svg'
+import cactus4 from '../../assets/garden/cactus4.svg'
+
 import purpleFlower33 from '../../assets/garden/purpleFlower/3petals3health.png'
 import purpleFlower32 from '../../assets/garden/purpleFlower/3petals2health.png'
 import purpleFlower31 from '../../assets/garden/purpleFlower/3petals1health.png'
@@ -104,7 +111,7 @@ const Garden = () => {
     }
   }
 
-  const setInventoryData = async (index) => {
+  const setInventoryData = async () => {
     try {
       const response = await fetch(`${BACKEND_URL}/garden/updateItems`, {
         method: 'POST',
@@ -142,12 +149,21 @@ const Garden = () => {
     var newInventory = inventory
     newInventory.seeds[pos].amount -= 1
     setInventory(newInventory)
-    setInventoryData(pos)
-    setMyPlants(getUpdatedGarden(pos))
-    setGardenData()
+    if (myPlants[holeClicked].fertilizer.getTime() >= new Date()) {
+      setMyPlants(plantAndGetGarden(pos, 3, 1))
+    } else {
+      setMyPlants(plantAndGetGarden(pos, 1, 1))
+    }
   }
 
-  const getUpdatedGarden = (pos, health, petals) => {
+  const fertilizeHole = () => {
+    var newInventory = inventory
+    newInventory.Abono -= 1
+    setInventory(newInventory)
+    setMyPlants(fertilizeAndGetGarden())
+  }
+
+  const plantAndGetGarden = (pos, health, petals) => {
     var result = myPlants
     result[holeClicked].health = health
     result[holeClicked].petals = petals
@@ -162,6 +178,12 @@ const Garden = () => {
         result[holeClicked].type = "white"
         break
     }
+    return result
+  }
+
+  const fertilizeAndGetGarden = () => {
+    var result = myPlants
+    result[holeClicked].fertilizer = new Date()
     return result
   }
 
@@ -270,68 +292,82 @@ const Garden = () => {
           }
         }
       default:
-        return dirt
+        return addIcon
     }
   }
 
     return (
       <View style={styles.container}>
-        <ImageBackground source={grass} resizeMode="cover" style={styles.image} />
-        <View style={styles.flor0}>
-          {
+        <Text>Mi jardín</Text>
+        <View style={styles.garden}>
+          <ImageBackground source={grass} resizeMode="cover" style={styles.image} />
+          <View style={styles.flor0}>
             {
-              "noflower": <BotonFlor id="0" image={getImage(0)} rounded={true} onClick={() => {setOpenSeedsMenu(true); setHoleClicked(0)}}/>
-            }[myPlants[0].type]
-            || <BotonFlor id="0" image={getImage(0)} rounded={false} onClick={() => {}}/>
-          }
-        </View>
-        <View style={styles.flor1}>
-        {
-            {
-              "noflower": <BotonFlor id="1" image={getImage(1)} rounded={true} onClick={() => {setOpenSeedsMenu(true), setHoleClicked(1)}}/>
-            }[myPlants[1].type]
-            || <BotonFlor id="1" image={getImage(1)} rounded={false} onClick={() => {}}/>
-          }
-        </View>
-        <View style={styles.flor2}>
-        {
-            {
-              "noflower": <BotonFlor id="2" image={getImage(2)} rounded={true} onClick={() => {setOpenSeedsMenu(true), setHoleClicked(2)}}/>
-            }[myPlants[2].type]
-            || <BotonFlor id="2" image={getImage(2)} rounded={false} onClick={() => {}}/>
-          }
-        </View>
-        <View style={styles.flor3}>
-        {
-            {
-              "noflower": <BotonFlor id="3" image={getImage(3)} rounded={true} onClick={() => {setOpenSeedsMenu(true), setHoleClicked(3)}}/>
-            }[myPlants[3].type]
-            || <BotonFlor id="3" image={getImage(3)} rounded={false} onClick={() => {}}/>
-          }
-        </View>
-        <Modal visible={openSeedsMenu}>
-          <View style={styles.modal}>
-            <Text style={styles.title} >Mis semillas</Text>
-            {inventory != null && inventory.seeds.map((element, index) => {
-              return (
-                <View style={styles.container} key={index}>
-                  <Text style={{alignSelf: 'flex-start'}} >{element.itemName}</Text>
-                  <Text style={{alignSelf: 'center'}} >{element.amount}</Text>
-                  <CustomButton onPress={() => {plantSeed(index, 1, 1); setOpenSeedsMenu(false)}} text='Plantar' type="cuaterciario" alignRight={true}/>
-                </View>
-              )
-            })}
-            <View style={styles.topRight}>
-              <BotonImagen image={close} onClick={() => setOpenSeedsMenu(false)} size='small'/>
-            </View>
+              {
+                "noflower": <BotonFlor id="0" image={getImage(0)} rounded={true} onClick={() => {setOpenSeedsMenu(true); setHoleClicked(0)}}/>
+              }[myPlants[0].type]
+              || <BotonFlor id="0" image={getImage(0)} rounded={false} onClick={() => {}}/>
+            }
           </View>
-        </Modal>
+          <View style={styles.flor1}>
+          {
+              {
+                "noflower": <BotonFlor id="1" image={getImage(1)} rounded={true} onClick={() => {setOpenSeedsMenu(true), setHoleClicked(1)}}/>
+              }[myPlants[1].type]
+              || <BotonFlor id="1" image={getImage(1)} rounded={false} onClick={() => {}}/>
+            }
+          </View>
+          <View style={styles.flor2}>
+          {
+              {
+                "noflower": <BotonFlor id="2" image={getImage(2)} rounded={true} onClick={() => {setOpenSeedsMenu(true), setHoleClicked(2)}}/>
+              }[myPlants[2].type]
+              || <BotonFlor id="2" image={getImage(2)} rounded={false} onClick={() => {}}/>
+            }
+          </View>
+          <View style={styles.flor3}>
+          {
+              {
+                "noflower": <BotonFlor id="3" image={getImage(3)} rounded={true} onClick={() => {setOpenSeedsMenu(true), setHoleClicked(3)}}/>
+              }[myPlants[3].type]
+              || <BotonFlor id="3" image={getImage(3)} rounded={false} onClick={() => {}}/>
+            }
+          </View>
+          <Modal visible={openSeedsMenu}>
+            <View style={styles.modal}>
+              <Text style={styles.title}>Mis objetos</Text>
+              {inventory != null && inventory.seeds.map((element, index) => {
+                return (
+                  <View style={styles.container} key={index}>
+                    <Text style={{alignSelf: 'flex-start'}} >{element.itemName}</Text>
+                    <Text style={{alignSelf: 'center'}} >{element.amount}</Text>
+                    <CustomButton onPress={() => {plantSeed(index, 1, 1); setOpenSeedsMenu(false)}} text='Plantar' type="cuaterciario" alignRight={true}/>
+                  </View>
+                )
+              })}
+              {inventory != null && 
+                <View style={styles.container} key={index}>
+                  <Text style={{alignSelf: 'flex-start'}} >Abono</Text>
+                  <Text style={{alignSelf: 'center'}} >{inventory.Abono}</Text>
+                  <CustomButton onPress={() => {fertilizeHole(index); setOpenSeedsMenu(false)}} text='Usar' type="cuaterciario" alignRight={true}/>
+                </View>
+              }
+              <View style={styles.topRight}>
+                <BotonImagen image={close} onClick={() => setOpenSeedsMenu(false)} size='small'/>
+              </View>
+            </View>
+          </Modal>
+        </View>
       </View>
     );
   }
   
   const styles = StyleSheet.create({
     container:{
+      flex: 1
+    },
+    garden:{
+      backgroundColor: '#F3FEFF',
       flex: 1
     },
     modal: {
