a275e8d38d78c08088e4c9b51728b92d8f65d22a
Jorge Sánchez <jorgesanlor1221@gmail.com>
2022-12-08 11:42:35 +0100

    Pruebas comercio

--- floorecer/spec/registroComercioSpec.js
+++ floorecer/spec/registroComercioSpec.js
@@ -5,6 +5,7 @@
       usernameForSearch: ["u", "us", "use", "user", "usern", "userna", "usernam", "username", "username1"],
       password: "Password1!",
       numberphone: "987654321",
+      isBusinessOwner: false,
       expectedStatus: 200,
       message: "User - Using correct data",
       eliminar: true
@@ -15,6 +16,7 @@
       usernameForSearch: ["u", "us", "use", "user", "usern", "userna", "usernam", "username", "username2"],
       password: "password",
       numberphone: "987654321",
+      isBusinessOwner: false,
       expectedStatus: 400,
       message: "User - Using weak password",
       eliminar: false
@@ -22,9 +24,10 @@
     {
       email: "",
       username: "",
-      usernameForSearch: ["u", "us", "use", "user", "usern", "userna", "usernam", "username", "username2"],
+      usernameForSearch: ["u"],
       password: "",
       numberphone: "",
+      isBusinessOwner: false,
       expectedStatus: 400,
       message: "User - Using empty data",
       eliminar: false
@@ -35,6 +38,7 @@
       usernameForSearch: ["u", "us", "use", "user", "usern", "userna", "usernam", "username", "username2"],
       password: "Password1!",
       numberphone: "987654321",
+      isBusinessOwner: false,
       expectedStatus: 400,
       message: "User - Using invalid email",
       eliminar: false
@@ -45,12 +49,13 @@
       usernameForSearch: ["u", "us", "use", "user", "usern", "userna", "usernam", "username", "username2"],
       password: "Password1!",
       numberphone: "9876543210",
+      isBusinessOwner: false,
       expectedStatus: 400,
       message: "User - Using invalid phone",
       eliminar: false
     },
   ].forEach((value) => {
     it(`${value.message} - Status code should be ${value.expectedStatus}`, async () => {
-      expect(await registrar(value.email, value.username, value.usernameForSearch, value.password, value.numberphone)).toBe(value.expectedStatus)
+      expect(await registrar(value.email, value.username, value.usernameForSearch, value.password, value.numberphone, value.isBusinessOwner)).toBe(value.expectedStatus)
     })
   })
--- floorecer/spec/registroUsuarioSpec.js
+++ floorecer/spec/registroUsuarioSpec.js
@@ -1,4 +1,4 @@
-const registrar = async (email, username, usernameForSearch, password, numerodetelefono) => {
+const registrar = async (email, username, usernameForSearch, password, numerodetelefono, isBusinessOwner) => {
   const response = await fetch(`http://192.168.0.72:5000/user-authe/userRegister`, {
     method: 'POST',
     body: JSON.stringify({
@@ -7,7 +7,7 @@ const registrar = async (email, username, usernameForSearch, password, numerodet
       usernameForSearch: usernameForSearch,
       password: password,
       numberphone : numerodetelefono,
-      isBusinessOwner : false,
+      isBusinessOwner : isBusinessOwner,
 
   }),
     headers: {
@@ -38,6 +38,7 @@ const registrar = async (email, username, usernameForSearch, password, numerodet
     usernameForSearch: ["u", "us", "use", "user", "usern", "userna", "usernam", "username", "username1"],
     password: "Password1!",
     numberphone: "987654321",
+    isBusinessOwner: false,
     expectedStatus: 200,
     message: "User - Using correct data",
     eliminar: true
@@ -48,6 +49,7 @@ const registrar = async (email, username, usernameForSearch, password, numerodet
     usernameForSearch: ["u", "us", "use", "user", "usern", "userna", "usernam", "username", "username1"],
     password: "password",
     numberphone: "987654321",
+    isBusinessOwner: false,
     expectedStatus: 400,
     message: "User - Using weak password",
     eliminar: false
@@ -55,9 +57,10 @@ const registrar = async (email, username, usernameForSearch, password, numerodet
   {
     email: "",
     username: "u",
-    usernameForSearch: ["u", "us", "use", "user", "usern", "userna", "usernam", "username", "username1"],
+    usernameForSearch: ["u"],
     password: "",
     numberphone: "",
+    isBusinessOwner: false,
     expectedStatus: 400,
     message: "User - Using empty data",
     eliminar: false
@@ -68,6 +71,7 @@ const registrar = async (email, username, usernameForSearch, password, numerodet
     usernameForSearch: ["u", "us", "use", "user", "usern", "userna", "usernam", "username", "username1"],
     password: "Password1!",
     numberphone: "987654321",
+    isBusinessOwner: false,
     expectedStatus: 400,
     message: "User - Using invalid email",
     eliminar: false
@@ -78,12 +82,13 @@ const registrar = async (email, username, usernameForSearch, password, numerodet
     usernameForSearch: ["u", "us", "use", "user", "usern", "userna", "usernam", "username", "username1"],
     password: "Password1!",
     numberphone: "9876543210",
+    isBusinessOwner: false,
     expectedStatus: 400,
     message: "User - Using invalid phone",
     eliminar: false
   },
 ].forEach((value) => {
   it(`${value.message} - Status code should be ${value.expectedStatus}`, async () => {
-    expect(await registrar(value.email, value.username, value.usernameForSearch, value.password, value.numberphone)).toBe(value.expectedStatus)
+    expect(await registrar(value.email, value.username, value.usernameForSearch, value.password, value.numberphone, value.isBusinessOwner)).toBe(value.expectedStatus)
   })
 })
